c1693c3b33dde0cf138421aa7c00bb983a6070d2
edwin <edwinkun@gmail.com>
2024-11-26 22:03:40 +0700

    initial commit :snake: :snail: :boar:

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Spring 2 and Infinispan 14 Library Mode with Prometheus Endpoint
+
+## Check Prometheus Endpoint
+```
+$ curl http://localhost:8080/actuator/prometheus
+```
---
+++ src/main/java/com/edw/config/InfinispanConfiguration.java
@@ -0,0 +1,44 @@
+package com.edw.config;
+
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfiguration;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.spring.starter.embedded.InfinispanCacheConfigurer;
+import org.infinispan.spring.starter.embedded.InfinispanGlobalConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+/**
+ * <pre>
+ *  com.edw.config.InfinispanConfiguration
+ * </pre>
+ *
+ * @author Muhammad Edwin < edwin at redhat dot com >
+ * 26 Nov 2024 21:51
+ */
+@Configuration
+public class InfinispanConfiguration {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public InfinispanCacheConfigurer infinispanCacheConfigurer() {
+        return manager -> {
+            org.infinispan.configuration.cache.Configuration userCacheConfig = new ConfigurationBuilder()
+                    .build();
+
+            manager.defineConfiguration("my-cache", userCacheConfig);
+        };
+    }
+
+    @Bean
+    public InfinispanGlobalConfigurer globalConfigurer() {
+        return () -> {
+            final GlobalConfiguration globalConfiguration = new GlobalConfigurationBuilder()
+                    .metrics().gauges(true).histograms(true)
+                    .build();
+            return globalConfiguration;
+        };
+    }
+}
